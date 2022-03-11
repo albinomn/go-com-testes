5a0a6d6030a9593155d8e1156bc564d5110de406
Albino Neto <albinomeneguetti@hotmail.com.br>
2022-03-10 23:14:02 -0300

    feat: iteration 	Simpler and better solution

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,11 @@
 package iteration
 
+const timesToRepeat = 5
+
 func Repeat(char string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + char
+	for i := 0; i < timesToRepeat; i++ {
+		repeated += char
 	}
 
 	return repeated
