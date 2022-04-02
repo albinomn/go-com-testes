515bbb8bf548be0e0a4a050204b7e2e4923328a9
Albino Neto <albinomeneguetti@hotmail.com.br>
2022-04-02 01:36:02 -0300

    feat: implemented interface for shapes to use it on tests

--- structs_methods_interfaces/smi.go
+++ structs_methods_interfaces/smi.go
@@ -1,14 +1,28 @@
 package smi
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+type Shape interface {
+	Area() float64
 }
 
 func Perimeter(rect Rectangle) float64 {
 	return 2 * (rect.width + rect.height)
 }
 
-func Area(rect Rectangle) float64 {
-	return rect.width * rect.height
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
 }
--- structs_methods_interfaces/smi_test.go
+++ structs_methods_interfaces/smi_test.go
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rect := Rectangle{12.0, 6.0}
-	got := Area(rect)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("\n Got: %.2f\n Want: %.2f", got, want)
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != want {
+			t.Errorf("\n Got: %g\n Want: %g", got, want)
+		}
 	}
+
+	t.Run("Rectangles", func(t *testing.T) {
+		rect := Rectangle{12.0, 6.0}
+		checkArea(t, rect, 72.0)
+	})
+
+	t.Run("Circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
