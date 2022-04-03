dcf63274375aca6e9b8fff9826352b69aeb336b1
Albino Neto <albinomeneguetti@hotmail.com.br>
2022-04-03 03:31:31 -0300

    feat:     Finished all tests and methods for Search

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -5,15 +5,35 @@ import (
 )
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("Known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertString(t, got, want)
+		assertString(t, got, want)
+	})
+
+	t.Run("Unkown word", func(t *testing.T) {
+		_, err := dictionary.Search("tester")
+
+		if err == nil {
+			t.Fatal("Expected to get errors")
+		}
+
+		assertError(t, err, ErrNotFound)
+	})
 }
 
 func assertString(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("\n Got:   %q\n Want:  %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
 	t.Helper()
 
 	if got != want {
