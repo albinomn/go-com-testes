7057296f5708ce6d2b689e6490a774ad8fc011b3
Albino Neto <albinomeneguetti@hotmail.com.br>
2022-05-13 02:48:10 -0300

    feat: Created a mock to test if the sleep and print are in order.

--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -2,23 +2,46 @@ package mocking
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
 
-	Countdown(buffer, spySleeper)
+		Countdown(buffer, spySleeper)
 
-	got := buffer.String()
-	want := "3\n2\n1\nGo!"
+		got := buffer.String()
+		want := "3\n2\n1\nGo!"
 
-	if got != want {
-		t.Errorf("\n Got:  %q\n Want: %q", got, want)
-	}
+		if got != want {
+			t.Errorf("\n Got:  %q\n Want: %q", got, want)
+		}
 
-	if spySleeper.Calls != 4 {
-		t.Errorf("Not enogh calls to sleeper\n Got: %d\n Want: 4", spySleeper.Calls)
-	}
+		if spySleeper.Calls != 4 {
+			t.Errorf("Not enogh calls to sleeper\n Got: %d\n Want: 4", spySleeper.Calls)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleeperPrinter := &SpyCountdownOperations{}
+		Countdown(spySleeperPrinter, spySleeperPrinter)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleeperPrinter.Calls) {
+			t.Errorf("\n Got:  %v\n Want: %v", spySleeperPrinter.Calls, want)
+		}
+	})
 }
--- mocking/sleeper.go
+++ mocking/sleeper.go
@@ -19,3 +19,19 @@ type SpySleeper struct {
 func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
+
+const write = "write"
+const sleep = "sleep"
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
