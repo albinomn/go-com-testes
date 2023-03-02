f989b3c83c9f3ab85394a8426ed2b3a07e220152
Albino Neto <albinomeneguetti@hotmail.com.br>
2023-03-01 23:47:36 -0300

    refactoring our map test case because Go doesn't guarantee order, and we assert our calls are done in a particular order

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -67,14 +67,6 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavík"},
 		},
-		{
-			"maps",
-			map[string]string{
-				"Foo": "Bar",
-				"Baz": "Boz",
-			},
-			[]string{"Bar", "Boz"},
-		},
 	}
 
 	for _, test := range cases {
@@ -88,5 +80,36 @@ func TestWalk(t *testing.T) {
 				t.Errorf("\n Got:  %q\n Want: %q", got, test.ExpectedCalls)
 			}
 		})
+	}
+
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+}
+
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+
+	contains := false
+
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+
+	if !contains {
+		t.Errorf("\n Expected %+v to contain %q, but it didn't", haystack, needle)
 	}
 }
